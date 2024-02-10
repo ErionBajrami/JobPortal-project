4d95a7e4ce1e5d69b1bdf57c3166438fe7d88f38
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2024-02-10 17:12:36 +0100

    setting up Redux store and keeping User Data

--- jobportal/pages/_app.js
+++ jobportal/pages/_app.js
@@ -1,37 +1,18 @@
 import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 import { store } from '@/Store/store'
 import { Provider } from 'react-redux'
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import Cookies from 'js-cookie';
-import 'react-toastify/dist/ReactToastify.css';
-
-
 
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-})
-{
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleBeforeUnload = (event) => {
-      event.preventDefault();
-      localStorage.removeItem("user")
-      Cookies.remove('token')
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, [router]);
+}) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </Provider>
   )
 }
