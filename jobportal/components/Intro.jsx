c96dd4639e0481ef31cd1d217e5f09936071df22
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2024-02-12 05:41:03 +0100

    Edited home page

--- jobportal/components/Intro.jsx
+++ jobportal/components/Intro.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function Intro() {
   return (
-    <div className='w-full h-full flex items-center flex-col justify-center text-indigo-600'>
-        <h1 className='font-semibold text-4xl mb-3 '>Empowering board members to </h1>
-        <h1 className='font-semibold text-4xl mb-3 '>make informed decisions </h1>
-        <h1 className='font-semibold text-lg mb-3 uppercase'>LOOKING FOR TALENT  OR SEARCHINg for job</h1>
-        <button className='px-4 py-2 border border-indigo-600 rounded uppercase tracking-widest mx-4   text-white bg-indigo-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-indigo-600'>get Started</button>
-    </div>
-  )
+    <>
+      <div
+        className='w-full h-screen bg-cover bg-center flex items-center justify-center flex-col'
+        style={{ backgroundImage: `url('/background.jpg')` }}
+      >
+        <div className='w-full h-full flex items-center justify-center py-24'>
+          <div className='w-2/3 h-full my-2 flex items-center justify-center text-center p-20 flex-col'>
+            <h1 className='text-6xl font-semibold mb-4 text-black'>
+             The Easiest Way to Get Your New Job
+            </h1>
+            <p className='text-lg mb-20 text-gray-400'>
+             Join Thousands of Companies That Rely on Jobify
+            </p>  
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
