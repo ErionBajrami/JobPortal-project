c98680f974665c321757a2a6a258177e2cdbf1b1
Erion Bajrami <94755488+ErionBajrami@users.noreply.github.com>
2024-02-14 18:27:21 +0100

    Fixed bugs and added uploading CV

--- jobportal/pages/api/job/applyJob.js
+++ jobportal/pages/api/job/applyJob.js
@@ -1,7 +1,9 @@
 import ConnectDB from '@/DB/connectDB';
-import AppliedJob from '@/models/ApplyJob';
 import Joi from 'joi';
-
+import AppliedJob from '@/models/ApplyJob';
+import formidable from 'formidable';
+import fs from 'fs';
+import path from 'path'
 
 const schema = Joi.object({
     name: Joi.string().required(),
@@ -14,29 +16,63 @@ const schema = Joi.object({
 export const config = {
     api: {
         bodyParser: false,
-    }
+    },
 };
+
+
+
 
 export default async (req, res) => {
     await ConnectDB();
-    console.log('i got hit successfully')
-    const data = req.body;
-    console.log(JSON.stringify(req.body))
+
+
+    try {
+        const form = new formidable.IncomingForm();
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                console.error('Error', err)
+                throw err
+            }
+            const oldPath = files.cv.filepath;
+            console.log(oldPath)
+            const fileName = files.cv.originalFilename;
+            console.log(fileName)
+            
+            const tempPath = path.join(process.cwd(), 'public', 'uploads', fileName + '.temp');
+            fs.renameSync(oldPath, tempPath);
+            fs.renameSync(tempPath, newPath);
+
+
+            const jobApplication = {
+                name: fields.name,
+                email: fields.email,
+                about: fields.about,
+                job: fields.job,
+                user: fields.user,
+                cv: fileName,
+            };
+
+            console.log(jobApplication)
+
+            const { error } = schema.validate(jobApplication);
+            if (error) return res.status(401).json({ success: false, message: error.details[0].message.replace(/['"]+/g, '') });
+
+            const newJobApplication = AppliedJob.create(jobApplication);
+            return res.status(200).json({ success: true, message: 'Job application submitted successfully' });
+
+
+        })
+    } catch (error) {
+
+        console.log('error in apply job (server) => ', error);
+        return res.status(500).json({ success: false, message: 'something went wrong' });
+    }
 
 
 
-    // const { name, email, job, user, about , cv} = data;
-    // const { error } = schema.validate({ name, email, job, user, about});
-
-    // if (error) return res.status(401).json({ success: false, message: error.details[0].message.replace(/['"]+/g, '') });
 
 
-    // try {
-    //     const creatingUser =  await AppliedJob.create({user , title,description , salary , company , email , job_category , job_type , job_experience , job_vacancy , job_deadline });
-    //     return res.status(200).json({ success: true, message: "Job Posted Successfully !" })
-    // } catch (error) {
-    //     console.log('Error in posting a job (server) => ', error);
-    //     return res.status(500).json({ success: false, message: "Something Went Wrong Please Retry Later !" })
-    // }
+
+
 }
 
--- jobportal/pages/frontend/applyJob/[id].jsx
+++ jobportal/pages/frontend/applyJob/[id].jsx
@@ -10,10 +10,13 @@ export default function ApplyJob() {
     const router = useRouter()
     const dispatch = useDispatch();
     const { id } = router.query
-    const user = useSelector(state => state.User.userData)
-    const [formData, setFormData] = useState({ name: '', email: "", about: '', job: id, user: user?._id, cv: null })
+    const activeUser = useSelector(state => state.User.userData)
+    const [formikData, setFormikData] = useState({ name: '', email: "", about: '', job: id, user: activeUser?._id })
+    const [file , setFile] = useState(null)
     const [error, setError] = useState({ name: '', email: "", about: '', job: '', user: '', cv: '' });
 
+
+    const { name, email, about, job, user} = formikData;
 
     const handleSubmit = async (e) => {
 
@@ -21,40 +24,53 @@ export default function ApplyJob() {
 
 
 
-
-        if (!formData.name) {
+        if (!name) {
             setError({ ...error, name: "Name Field is required" })
             return;
         }
 
-        if (!formData.email) {
+        if (!email) {
             setError({ ...error, email: "Email Field is required" })
             return;
         }
 
-
-        if (!formData.user) {
+        if (!user) {
             return toast.error('Please Login First')
         }
-        if (!formData.job) {
+
+        if (!job) {
             return toast.error('Please Follow Apply Process ')
         }
-        if (!formData.about) {
+
+        if (!about) {
             setError({ ...error, about: "About Field is required" })
             return;
         }
-        if (!formData.cv) {
+
+        if (!file) {
             setError({ ...error, cv: "Please Upload CV" })
             return;
         }
-        console.log(formData)
 
-        const res = await apply_job(formData);
-        // if (res.success) {
-        //     toast.success(res.message)
-        // } else {
-        //     toast.error(res.message)
-        // }
+        
+
+        const form = new FormData();
+        form.append('name', name);
+        form.append('email', email);
+        form.append('about', about);
+        form.append('job', job);
+        form.append('user', user);
+        form.append('cv', file);
+
+
+        const res = await apply_job(form);
+        if (res.success) {
+            toast.success('Your Application is Submitted')
+        } else {
+            toast.error('Something Went Wrong')
+        }
+
+     
 
     }
 
@@ -66,28 +82,28 @@ export default function ApplyJob() {
                 <form encType="multipart/form-data" onSubmit={handleSubmit} className="sm:w-1/2 w-full px-4 mx-4  h-full" >
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="title" className='mb-1 text-base font-semibold'>Name :</label>
-                        <input onChange={(e) => setFormData({ ...formData, name: e.target.value })} type="text" id='title' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Name ' />
+                        <input name='name' onChange={(e) => setFormikData({ ...formikData, name: e.target.value })} type="text" id='title' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Name ' />
                         {
                             error.name && <p className="text-sm text-red-500">{error.name}</p>
                         }
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="email" className='mb-1 text-base font-semibold'>Email :</label>
-                        <input onChange={(e) => setFormData({ ...formData, email: e.target.value })} type="email" id='email' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Email' />
+                        <input name='email' onChange={(e) => setFormikData({ ...formikData, email: e.target.value })} type="email" id='email' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Email' />
                         {
                             error.email && <p className="text-sm text-red-500">{error.email}</p>
                         }
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="description" className='mb-1 text-base font-semibold'>About :</label>
-                        <textarea onChange={(e) => setFormData({ ...formData, about: e.target.value })} type="description" id='description' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter description' />
+                        <textarea name='about' onChange={(e) => setFormikData({ ...formikData, about: e.target.value })} type="description" id='description' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter description' />
                         {
                             error.about && <p className="text-sm text-red-500">{error.about}</p>
                         }
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="file" className='mb-1 text-base font-semibold'>Upload CV :</label>
-                        <input name='cv' onChange={(e) => setFormData({ ...formData, cv: e.target.files[0] })} type="file" id='file' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Email' />
+                        <input  name='cv' onChange={(e) => setFile(e.target.files[0])} type="file" id='file' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Email' />
                         {
                             error.cv && <p className="text-sm text-red-500">{error.cv}</p>
                         }
