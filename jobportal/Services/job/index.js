afceba80d24c5c5838d39e1f2031339209dfef67
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2024-02-14 18:14:51 +0100

    Implemented apply job page and api to apply

--- jobportal/Services/job/index.js
+++ jobportal/Services/job/index.js
@@ -1,5 +1,7 @@
 
+
 export const post_job = async (formData) => {
+
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/job/postAJob`, {
             method: 'POST',
@@ -38,5 +40,25 @@ export const get_specified_job = async (id) => {
         return data;
     } catch (error) {
         console.log('error in getting  specified job (service) => ', error);
+    }
+}
+
+
+
+export const apply_job = async (formData) => {
+    const { name, email, about, job, user, cv } = formData;
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/job/applyJob`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+            body: formData,
+        });
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log('error in apply job (service) => ', error);
     }
 }
---
+++ jobportal/pages/api/job/applyJob.js
@@ -0,0 +1,42 @@
+import ConnectDB from '@/DB/connectDB';
+import AppliedJob from '@/models/ApplyJob';
+import Joi from 'joi';
+
+
+const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    about: Joi.string().required(),
+    job: Joi.string().required(),
+    user: Joi.string().required(),
+});
+
+export const config = {
+    api: {
+        bodyParser: false,
+    }
+};
+
+export default async (req, res) => {
+    await ConnectDB();
+    console.log('i got hit successfully')
+    const data = req.body;
+    console.log(JSON.stringify(req.body))
+
+
+
+    // const { name, email, job, user, about , cv} = data;
+    // const { error } = schema.validate({ name, email, job, user, about});
+
+    // if (error) return res.status(401).json({ success: false, message: error.details[0].message.replace(/['"]+/g, '') });
+
+
+    // try {
+    //     const creatingUser =  await AppliedJob.create({user , title,description , salary , company , email , job_category , job_type , job_experience , job_vacancy , job_deadline });
+    //     return res.status(200).json({ success: true, message: "Job Posted Successfully !" })
+    // } catch (error) {
+    //     console.log('Error in posting a job (server) => ', error);
+    //     return res.status(500).json({ success: false, message: "Something Went Wrong Please Retry Later !" })
+    // }
+}
+
